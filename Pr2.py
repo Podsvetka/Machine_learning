a2e14463b3de4b9a6160e1a3919a4f724e59d022
Podsvetka <s.y.topchiy@student.khai.edu>
2023-11-22 16:18:34 +0200

    Пункт 1

---
+++ Pr2.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+# Створення вектора (1, 2, 3)
+vector = np.array([1, 2, 3])
+
+# Виведення вектора і типу масиву
+print("Вектор:", vector)
+print("Тип масиву:", type(vector))
+
+
